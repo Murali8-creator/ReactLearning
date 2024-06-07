9c0044ebb0059749ed60ef9ffdf41d56bf25f70e
Murali8-creator <muralig436@gmail.com>
2024-06-07 14:09:05 +0530

    Validating Input Upon Lost Focus

--- Forms/src/components/StateLogin.jsx
+++ Forms/src/components/StateLogin.jsx
@@ -1,4 +1,96 @@
-const [enteredValues, setEnteredValues] = useState({
-    email:'',
-    password:''
+import { useState } from 'react';
+
+export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
   });
+
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  })
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes('@');
+  const passwordIsInvalid =didEdit.password && enteredValues.password.trim().length < 8;
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(enteredValues);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+
+    setDidEdit((prevEdit) => ({
+        ...prevEdit,
+        [identifier]: false,
+    }))
+  }
+
+  function handleInputBlur(identifier){
+    setDidEdit(prevEdit => (
+        {
+            ...prevEdit,
+            [identifier]: true,
+        }
+    ))
+  }
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
+
+  // function handlePasswordChange(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            onBlur={() => handleInputBlur('email')}
+            value={enteredValues.email}
+          />
+          <div className="control-error">{emailIsInvalid && <p>Email is Invalid</p>}</div>
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+            onBlur={() => handleInputBlur('password')}
+            value={enteredValues.password}
+          />
+          <div className='control-error'>{passwordIsInvalid && <p>Invalid Password</p>}</div>
+        </div>
+        
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
