374f43ee2be8f1f6b0cd5c8ad7cad954a5eb7181
Murali8-creator <muralig436@gmail.com>
2024-06-07 14:48:01 +0530

    validating input upon form submission

--- Forms/src/components/Login.jsx
+++ Forms/src/components/Login.jsx
@@ -1,7 +1,14 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 
 export default function Login() {
+
+  // const [emailIsInvalid, setEmailIsInvalid]=useState();
+
+  const [invalid, setInvalid] = useState({
+    email: false,
+    password: false,
+  });
 
    const emailRef = useRef();
    const passwordRef = useRef();
@@ -10,7 +17,25 @@ export default function Login() {
      event.preventDefault();
      const email = emailRef.current.value;
      const password = passwordRef.current.value;
-     console.log(email, password);
+     
+     const emailIsInvalid = !email.includes('@');
+     const passwordIsInvalid = password.trim().length < 8;
+
+     if(emailIsInvalid){
+      setInvalid((prevInvalid) => ({
+        ...prevInvalid,
+        email: true,
+      }));
+      
+     }
+
+      if(passwordIsInvalid){
+        setInvalid((prevInvalid) => ({
+          ...prevInvalid,
+          password: true,
+        }));
+      }
+
     }
  
   return (
@@ -21,11 +46,13 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={emailRef}/>
+          <div className="control-error">{invalid.email && <p>Invalid Email</p>}</div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
           <input id="password" type="password" name="password" ref={passwordRef}/>
+          <div className="control-error">{invalid.password && <p>Invalid Password</p>}</div>
         </div>
       </div>
 
